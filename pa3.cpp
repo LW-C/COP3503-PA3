a1b78873b103eda38ef9dbfa01753ae2b087f2aa
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2018-04-06 13:54:34 -0400

    Implemented the methods in the .h

--- pa3.cpp
+++ pa3.cpp
@@ -1,14 +1,17 @@
+/*
+ *  Program written by Lauren Wonicker-Cook
+ *  for COP3503 Programming Assignment #3.
+ */
+
 #include <iostream>
 #include "pa3.h"
 #include <fstream>
-
 
 /*
  *  Open the file reading class in the main method
  *  and pass it as a parameter to the methods that
  *  will open and read the file.
  */
-
 
 std::ifstream getFile()
 {
@@ -109,6 +112,120 @@ void printOutput(int depth, Stack* theStack)
     std::cout << "\n";
 }
 
+Stack::Stack()
+{
+}
+
+void Stack::push(std::string a)
+{
+    /*
+     *  This method will check to see which
+     *  vector the string belongs in.
+     */
+}
+
+void Stack::pushKeyword(std::string a)
+{
+    keywords.push_back(a);
+}
+
+void Stack::pushIdentifier(std::string a)
+{
+    identifiers.push_back(a);
+}
+
+void Stack::pushConstant(std::string a)
+{
+    constants.push_back(a);
+}
+
+void Stack::pushOperator(std::string a)
+{
+    operators.push_back(a);
+}
+
+void Stack::pushDelimiter(std::string a)
+{
+    delimiters.push_back(a);
+}
+
+void Stack::pushSyntaxError(std::string a)
+{
+    syntaxErrors.push_back(a);
+}
+
+std::string Stack::popKeyword()
+{
+    std::string a = keywords.back();
+    keywords.pop_back();
+    return a;
+}
+
+std::string Stack::popIdentifier()
+{
+    std::string a = identifiers.back();
+    identifiers.pop_back();
+    return a;
+}
+
+std::string Stack::popConstant()
+{
+    std::string a = constants.back();
+    constants.pop_back();
+    return a;
+}
+
+std::string Stack::popOperator()
+{
+    std::string a = operators.back();
+    operators.pop_back();
+    return a;
+}
+
+std::string Stack::popDelimiter()
+{
+    std::string a = delimiters.back();
+    delimiters.pop_back();
+    return a;
+}
+
+std::string Stack::popSyntaxError()
+{
+    std::string a = syntaxErrors.back();
+    syntaxErrors.pop_back();
+    return a;
+}
+
+int Stack::getKeywordSize()
+{
+    return static_cast<int>(keywords.size());
+}
+
+int Stack::getIdentifierSize()
+{
+    return static_cast<int>(identifiers.size());
+}
+
+int Stack::getConstantSize()
+{
+    return static_cast<int>(constants.size());
+}
+
+int Stack::getOperatorSize()
+{
+    return static_cast<int>(operators.size());
+}
+
+int Stack::getDelimiterSize()
+{
+    return static_cast<int>(delimiters.size());
+}
+
+int Stack::getSyntaxErrorSize()
+{
+    return static_cast<int>(syntaxErrors.size());
+}
+
 int main() {
     // Call getFile
     /*
@@ -117,5 +234,8 @@ int main() {
      *  the program.
      */
     // Call readFile
+    Stack* theStack = new Stack();
+    theStack->pushKeyword("Test");
+    theStack->popKeyword();
     return 0;
 }
