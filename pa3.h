c9f55dd3753c5ee4e7ac36e08314c91d7058fe66
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2018-04-06 13:53:16 -0400

    Fixed a few typos

--- pa3.h
+++ pa3.h
@@ -10,19 +10,19 @@ private:
     std::vector<std::string> identifiers;
     std::vector<std::string> constants;
     std::vector<std::string> operators;
-    std::vector<std::string> delimiteres;
+    std::vector<std::string> delimiters;
     std::vector<std::string> syntaxErrors;
 public:
     Stack();
-    void push(std::string);             //Calls the other methods to push a string to the end of the appropriate vector
-    void pushKeyword(std::string);
-    void pushIdentifier(std::string);
-    void pushConstant(std::string);
-    void pushOperator(std::string);
-    void pushDelimiter(std::string);
-    void pushSyntaxError(std::string);
-    std::string pop();                  //Calls the other methods to pop a string from the end of the appropriate vector
-                                        //Is this method necessary?
+    void push(std::string a);             //Calls the other methods to push a string to the end of the appropriate vector
+    void pushKeyword(std::string a);
+    void pushIdentifier(std::string a);
+    void pushConstant(std::string a);
+    void pushOperator(std::string a);
+    void pushDelimiter(std::string a);
+    void pushSyntaxError(std::string a);
+    /*std::string pop();                  //Calls the other methods to pop a string from the end of the appropriate vector
+                                        //Is this method necessary?*/
     std::string popKeyword();
     std::string popIdentifier();
     std::string popConstant();
