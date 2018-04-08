d177a024a22df7ff79c48e425b6ee8059e41f849
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2018-04-08 12:25:14 -0400

    Adding the compiler's file for debugging

---
+++ cmake-build-debug/CMakeFiles/pa3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa3.dir/pa3.cpp.o"
+  "pa3.pdb"
+  "pa3.exe"
+  "libpa3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
